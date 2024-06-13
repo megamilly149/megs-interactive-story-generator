8549e2e6b8759a2da9a5c0c20b1866bad952d1c5
Meagan <meaganprice@me.com>
2024-06-13 12:55:23 -0400

    add ping test

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ require("dotenv").config();
 const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-  ],
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+    	GatewayIntentBits.MessageContent,
+    ],
 });
 
 // listen to the event that signals bot is ready to start working
 client.on("ready", () => console.log(`logged in as ${client.user.tag}`));
+
+// message handling
+client.on('messageCreate', message => {
+    if (message.content === 'ping') {
+        message.channel.send('Pong!');
+    }
+});
 
 // login to the server using bot token
 client.login(process.env.TOKEN);
